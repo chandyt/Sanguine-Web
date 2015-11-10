5d5513851f9e180b35f4da1b1b09c114b4f0c620
bishalgautam <bishal.htd04@gmail.com>
2015-11-10 13:26:45 -0500

    search function currently in table User Details

--- search.php
+++ search.php
@@ -1,37 +1,49 @@
-
+      
 <?php 
+
 include('dbconnect.php');
 
 if($_POST)
 {
-	$searchId = $_POST['searchId']
-
-// Select a database:
-mssql_select_db('Sanguine') 
-		    or die('Could not select a database.');
-
-// Example query: (TOP 10 equal LIMIT 0,10 in MySQL)
-$SQL = "SELECT * FROM [Sanguine].[dbo].[UserDetail]  
-        where PhoneNumber = $searchId or UserName = $searchId "; 
-
-// Execute query:
-$result = mssql_query($SQL) 
-    or die('A error occured: ' . mysql_error());
-
- 
-// Get result count:
-$Count = mssql_num_rows($result);
-print "Showing $count rows:<hr/>\n\n";   
+	$searchId = $_POST['userId'];
 
 
+	//$sql = "SElECT UserName, PhoneNumber, Name , Email , Type FROM [UserDetail]  INNER JOIN [BloodType] ON Username"
 
-// Fetch rows:
-while ($Row = mssql_fetch_assoc($result)) {
- 
-    print $Row['Fieldname'] . "\n";
+	$sql = "SELECT * FROM [UserDetail] WHERE UserName = '$searchId' OR PhoneNumber = '$searchId'";
 
+	      /*'$searchId' or PhoneNumber = '$searchId' " 
+       		or Username = ? "*/
+
+        //$params = array($searchId);
+        
+	$stmt = sqlsrv_query( $conn, $sql);
+
+	if( $stmt === false) {
+	echo "false";
+    die( print_r( sqlsrv_errors(), true) );
+	}
+
+	//$row_count = sqlsrv_num_rows( $stmt ); 
+
+    		//if ($row_count === false)
+   			//		echo "Error in retrieveing row count.";
+   			//else
+   				//echo $row_count;
+
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+	echo $row['Name'].", ".$row['PhoneNumber'].",".$row['UserName'].",".$row['Email']." <br />";
+	
+	}
+
+
+//header('Location: search_result.html');
+
+sqlsrv_free_stmt( $stmt);    
 
 sqlsrv_close( $conn);
+ 
 }
-
 ?>
+
+
